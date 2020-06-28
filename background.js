45434dc61b59f150da1a50507ed5368e2fe9195c
David Sweetman <davidsweetman@live.ie>
2020-06-28 15:32:52 +0100

    Update script to duplicate mapped keys

--- background.js
+++ background.js
@@ -1,2 +1,22 @@
 console.log("content_script.js");
+window.addEventListener("keyup", myFunc);
 
+let mappings = {
+    "\"" : "\"",
+    "\(" : "\)",
+    "\{" : "\}",
+};
+
+function myFunc(e) {
+
+    for (let m in mappings){
+        if (e.key === m){
+            let tval = e.target.value;
+            let caret = e.target.selectionStart;
+            e.target.value = tval + mappings[m];
+            e.target.selectionEnd = caret;
+        }
+
+    }
+
+}
