99a706e212743c9f3e6f1489ae296f3d917bb43f
David Sweetman <davidsweetman@live.ie>
2020-06-28 15:46:21 +0100

    Comments and additional mapping

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 console.log("content_script.js");
 window.addEventListener("keyup", duplicate);
 
+// Map of values to listen for in text input along with the corresponding value to add
 let mappings = {
     "\"" : "\"",
     "\(" : "\)",
     "\{" : "\}",
+    "\[" : "\]",
 };
 
+//Function to add corresponding value when keypress event value is one of mapped values
 function duplicate(keyPress) {
 
     for (let m in mappings){
@@ -14,6 +17,7 @@ function duplicate(keyPress) {
             let textval = keyPress.target.value;
             let caretPosition = keyPress.target.selectionStart;
             keyPress.target.value = textval + mappings[m];
+            //Reset caret position to previous position after character is added
             keyPress.target.selectionEnd = caretPosition;
         }
 
